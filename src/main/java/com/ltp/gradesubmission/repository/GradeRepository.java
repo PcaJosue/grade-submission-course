b9a563a32e96df1b3029b8f7d02dff239053d663
Josue Pacheco <pca.josue@gmail.com>
2024-06-06 11:56:02 -0500

    adding beans

--- src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
+++ src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -3,25 +3,27 @@ package com.ltp.gradesubmission.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.ltp.gradesubmission.Grade;
-
+@Repository
 public class GradeRepository {
-    private List<Grade> studentGrades = new ArrayList<Grade>();
+  private List<Grade> studentGrades = new ArrayList<Grade>();
 
-    public Grade getGrade(int index){
-      return studentGrades.get(index);
-    }
+  public Grade getGrade(int index) {
+    return studentGrades.get(index);
+  }
 
-    public void addGrade(Grade grade){
-      studentGrades.add(grade);
-    }
+  public void addGrade(Grade grade) {
+    studentGrades.add(grade);
+  }
 
-    public void updateGrade(Grade grade, int index){
-      studentGrades.set(index,grade);
-    }
+  public void updateGrade(Grade grade, int index) {
+    studentGrades.set(index, grade);
+  }
 
-    public List<Grade> getGrades(){
-      return studentGrades;
-    }
+  public List<Grade> getGrades() {
+    return studentGrades;
+  }
 
 }
